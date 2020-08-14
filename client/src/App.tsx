d2b4fd3a88e5d159687988307d28cc6f35d20bc4
Brandon Endres <17bendres@gmail.com>
2020-08-14 11:39:14 -0400

    Adds frontend boilerplate

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <span>Florida Poly Rocket League V2</span>
+      <p>
+        Dashboard Management Application for the Florida Poly Rocket League Team
+      </p>
+    </div>
+  );
+}
+
+export default App;
