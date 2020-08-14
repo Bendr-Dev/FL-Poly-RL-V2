61daff547cb869d685f5507af9d83babc46ae0b1
Brandon Endres <17bendres@gmail.com>
2020-08-14 14:13:02 -0400

    Adds User model

---
+++ common/interfaces/User.Interface.ts
@@ -0,0 +1,14 @@
+import { Mongoose } from "mongoose";
+
+export interface IUser {
+  // _id?: string;
+  username: string;
+  email: string;
+  name: string;
+  steam64Id: string;
+  discordId: string;
+  role:
+    | Set<"Guest" | "Player" | "Coach" | "Manager" | "Admin">
+    | Array<"Guest" | "Player" | "Coach" | "Manager" | "Admin">;
+  password?: string;
+}
