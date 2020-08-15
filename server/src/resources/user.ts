91d82428aa57a54c38f74c279c62d88f47079c19
Brandon Endres <17bendres@gmail.com>
2020-08-15 12:19:20 -0400

    Adds validation, JWT token

--- server/src/resources/user.ts
+++ server/src/resources/user.ts
@@ -1,40 +1,94 @@
 import express, { Request, Response } from "express";
 import User from "../models/User";
+import { check, validationResult } from "express-validator";
+import * as bcrypt from "bcryptjs";
+import * as jwt from "jsonwebtoken";
+import * as dotenv from "dotenv";
 
 const userRouter = express.Router();
+
+dotenv.config();
+
+const JWTSecret = process.env.JWT_SECRET;
+const isDev = process.env.IS_DEV;
+
+const expiresIn = isDev ? 360000 : 14400;
 
 /**
  * Route registering a single user
  * @name POST/register
  */
-userRouter.post("/register", async (req: Request, res: Response) => {
-  try {
-    // Deconstruct request
-    const {
-      username,
-      name,
-      email,
-      password,
-      discordId,
-      steam64Id,
-      role,
-    } = req.body;
+userRouter.post(
+  "/register",
+  [
+    check("username", "Username is required").not().isEmpty(),
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password must contain atleast 6 characters").isLength({
+      min: 6,
+    }),
+    check("discordId", "Discord ID is required").not().isEmpty(),
+    check("steam64Id", "Steam64ID is required").not().isEmpty(),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-    // Create a new User object
-    const newUser = new User({
-      username,
-      name,
-      email,
-      password,
-      discordId,
-      steam64Id,
-      role,
-    });
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors);
+    }
 
-    res.status(201).json(newUser);
-  } catch (err) {
-    console.error(err);
+    try {
+      // Deconstruct request
+      const {
+        username,
+        name,
+        email,
+        password,
+        discordId,
+        steam64Id,
+        role,
+      } = req.body;
+
+      // Check if email already exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Create a new User object
+      const newUser = new User({
+        username,
+        name,
+        email,
+        password,
+        discordId,
+        steam64Id,
+        role,
+      });
+
+      // Encrypt password
+      newUser.password = await bcrypt.hash(password, 10);
+
+      // Save to DB
+      await newUser.save();
+
+      // Return webtoken
+      const payload = {
+        id: newUser.id,
+      };
+
+      jwt.sign(payload, JWTSecret as string, { expiresIn }, (err, token) => {
+        if (err) throw err;
+        res.status(201).json({ token });
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Server Error");
+    }
   }
-});
+);
 
 export default userRouter;
--- server/src/utils/db.ts
+++ server/src/utils/db.ts
@@ -6,10 +6,15 @@ dotenv.config();
 const pass = process.env.MONGO_PASS;
 const user = process.env.MONGO_USERNAME;
 const dbName = process.env.DB_NAME;
+const isDev = process.env.IS_DEV;
 
 export default async () => {
   try {
     console.log("Connecting to MongoDB...");
+
+    if (isDev && mongoose.connection) {
+      mongoose.connection.close();
+    }
 
     await mongoose.connect(
       `mongodb+srv://${user}:${pass}@fl-poly-rl-dev.czctl.mongodb.net/${dbName}?retryWrites=true&w=majority`,
