0fd9fa9be21b59617687a0ee4fccdedd54002a2c
Brandon Endres <17bendres@gmail.com>
2020-08-16 21:52:00 -0400

    Adds uploader property

--- common/interfaces/Event.Interface.ts
+++ common/interfaces/Event.Interface.ts
@@ -3,7 +3,14 @@ import { IUser } from "./User.Interface";
 export interface IEvent {
   type: string;
   format: string;
-  attending: Pick<IUser, "email" | "discordId" | "name" | "steam64Id">;
+  attending: Pick<
+    IUser,
+    "email" | "discordId" | "name" | "steam64Id" | "username"
+  >[];
+  uploader: Pick<
+    IUser,
+    "email" | "discordId" | "name" | "steam64Id" | "username"
+  >;
   link: string;
   time: Date;
 }
