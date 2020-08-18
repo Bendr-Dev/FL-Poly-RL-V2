690f0f8942fe1db8dc737dd0bd8eb1b751110704
Casey White <cwhite2019@gmail.com>
2020-08-17 23:45:55 -0400

    adds date

---
+++ client/src/utils/date.ts
@@ -0,0 +1,83 @@
+export const DATE_MAP: {
+  [key: number]: any;
+} = {
+  0: {
+    long: "January",
+    short: "Jan",
+  },
+  1: {
+    long: "Febuary",
+    short: "Feb",
+  },
+  2: {
+    long: "March",
+    short: "Mar",
+  },
+  3: {
+    long: "April",
+    short: "Apr",
+  },
+  4: {
+    long: "May",
+    short: "May",
+  },
+  5: {
+    long: "June",
+    short: "Jan",
+  },
+  6: {
+    long: "July",
+    short: "Jul",
+  },
+  7: {
+    long: "August",
+    short: "Aug",
+  },
+  8: {
+    long: "September",
+    short: "Sep",
+  },
+  9: {
+    long: "October",
+    short: "Oct",
+  },
+  10: {
+    long: "November",
+    short: "Nov",
+  },
+  11: {
+    long: "December",
+    short: "Dec",
+  },
+};
+
+export const DAYS_OF_WEEK = {
+  0: {
+    long: "Sunday",
+    short: "Sun",
+  },
+  1: {
+    long: "Monday",
+    short: "Mon",
+  },
+  2: {
+    long: "Tuesday",
+    short: "Tue",
+  },
+  3: {
+    long: "Wednesday",
+    short: "Wed",
+  },
+  4: {
+    long: "Thursday",
+    short: "Thu",
+  },
+  5: {
+    long: "Friday",
+    short: "Fri",
+  },
+  6: {
+    long: "Saturday",
+    short: "Sat",
+  },
+};
