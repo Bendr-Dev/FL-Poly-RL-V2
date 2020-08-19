e6d4b65b0e35c35abcfb8542fcc7eb7fa19c7c30
Brandon Endres <17bendres@gmail.com>
2020-08-19 15:56:10 -0400

    Init login component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import "./App.css";
 import Navbar from "./layout/Navbar";
 import Sidebar from "./layout/Sidebar";
 import Dashboard from "./dashboard/Dashboard";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./auth/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar></Navbar>
-      <div className="content-area">
-        <Sidebar></Sidebar>
-        <Dashboard></Dashboard>
-      </div>
-    </div>
-  );
+export interface IAuthState {
+  isLoggedIn: boolean;
+  user: any;
 }
 
-export default App;
+export const AuthContext = createContext([{}, () => {}] as [IAuthState, any]);
+
+export default () => {
+  const [authState, setAuthState] = useState({
+    isLoggedIn: false,
+    user: {},
+  });
+  return (
+    <AuthContext.Provider value={[authState, setAuthState]}>
+      <Router>
+        <div className="App">
+          <Navbar></Navbar>
+          <div className="content-area">
+            <Sidebar></Sidebar>
+            <Switch>
+              <ProtectedRoute
+                exact
+                path="/dashboard"
+                component={Dashboard}
+                isLoggedIn={authState.isLoggedIn}
+              />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    </AuthContext.Provider>
+  );
+};
