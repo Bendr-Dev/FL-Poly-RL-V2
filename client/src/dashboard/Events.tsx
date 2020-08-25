49d05262661d6420998d8f2c664abae13df89ac1
Brandon Endres <17bendres@gmail.com>
2020-08-24 21:01:14 -0400

    Updates UI on weekly events

--- client/src/dashboard/Events.tsx
+++ client/src/dashboard/Events.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, Fragment } from "react";
 import { DateContext } from "./Dashboard";
 import { getData } from "../utils/http";
 import { DAYS_OF_WEEK } from "../utils/date";
@@ -28,20 +28,44 @@ export default () => {
     data();
   }, [dateState]);
 
+  const formatTime = (time: Date): string => {
+    let hours = new Date(time).getHours();
+    const minutes = new Date(time).getMinutes();
+    let dayStatus: "PM" | "AM";
+
+    if (hours >= 12 && hours !== 24) {
+      dayStatus = "PM";
+    } else {
+      dayStatus = "AM";
+    }
+
+    if (hours > 12) {
+      hours = hours - 12;
+    }
+
+    return `${hours}:${minutes} ${dayStatus}`;
+  };
+
   return (
     <div className="weekly-events">
       <div className="weekly-header">
         <h2>Weekly Events</h2>
-        <span>
-          {dateState.startDate?.toLocaleDateString()} to{" "}
-          {dateState.endDate?.toLocaleDateString()}
-        </span>
-        <div>Button here</div>
+        {dateState.startDate || dateState.endDate ? (
+          <span>
+            {dateState.startDate?.toLocaleDateString()} to{" "}
+            {dateState.endDate?.toLocaleDateString()}
+          </span>
+        ) : (
+          <span></span>
+        )}
+
+        <div className="btn-small">+ Create Event</div>
       </div>
       <div className="line-break-primary"></div>
       <div className="weekly-body">
-        {!!events &&
-          events.map((eventDay: any) => {
+        {dateState.startDate &&
+          !!events &&
+          events.map((eventDay: any, index: number) => {
             const date = Object.keys(eventDay)[0];
             const day = new Date(date);
             day.setHours(25);
@@ -49,19 +73,23 @@ export default () => {
             !!Object.values(eventDay)[0] &&
               (items = Object.values(eventDay)[0]);
             return (
-              <div key={date} className="weekly-day">
-                <div className="day-header">
-                  {DAYS_OF_WEEK[day.getDay()]["long"]}
+              <Fragment key={date}>
+                <div className="weekly-day">
+                  <div className="day-header">
+                    {DAYS_OF_WEEK[day.getDay()]["long"]}
+                  </div>
+                  {!!Object.values(eventDay)[0] &&
+                    items.map((item: any, j: number) => {
+                      return (
+                        <div className="weekly-event" key={j}>
+                          <span>{formatTime(item.time)}</span>{" "}
+                          <div>{item.type}</div>
+                        </div>
+                      );
+                    })}
                 </div>
-                {!!Object.values(eventDay)[0] &&
-                  items.map((item: any, index: number) => {
-                    return (
-                      <div className="weekly-event" key={index}>
-                        {item.type}
-                      </div>
-                    );
-                  })}
-              </div>
+                {index !== 6 ? <div className="vertical-divider"></div> : ""}
+              </Fragment>
             );
           })}
       </div>
